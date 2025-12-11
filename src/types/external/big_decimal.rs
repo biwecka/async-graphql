18d61be285ad62219e20e055f0f048a95e4009cd
Luca Quaer <luca.quaer@ascatu.de>
2025-12-11 21:06:59 +0100

    feat: disable float inputs for 'bigdecimal'

--- src/types/external/big_decimal.rs
+++ src/types/external/big_decimal.rs
@@ -10,52 +10,10 @@ impl ScalarType for BigDecimal {
         println!("[BigDecimal] value={value:?}");
 
         match &value {
-            Value::Number(n) => {
-                // println!("[BigDecimal] value is a number");
-
-                // if let Some(f) = n.as_f64() {
-                //     let val = BigDecimal::try_from(f).map_err(InputValueError::custom)?;
-                //     println!("[BigDecimal] value is a f64: {f} => {val:?}");
-                //     return Ok(val);
-                // }
-
-                // if let Some(f) = n.as_i64() {
-                //     let val = BigDecimal::from(f);
-                //     println!("[BigDecimal] value is a i64: {f} => {val:?}");
-                //     return Ok(val);
-                // }
-
-                // let n2 = n.as_u64().unwrap();
-                // let val = BigDecimal::from(n2);
-
-                // println!("[BigDecimal] value is 'probably' a u64: {n2} => {val:?}");
-
-                let val = BigDecimal::from_str(n.as_str())?;
-                println!(
-                    "[BigDecimal] value is a arbitrary-precision-num '{}' => {:?}",
-                    n.as_str(),
-                    val
-                );
-
-                // unwrap safe here, because we have check the other possibility
-                Ok(val)
-            }
             Value::String(s) => {
                 let val = BigDecimal::from_str(s)?;
                 println!("[BigDecimal] value is a string '{s}' => {:?}", val);
                 Ok(val)
-            },
-
-            Value::Object(o) => {
-                match o.get("$serde_json::private::Number") {
-                    Some(async_graphql_value::ConstValue::String(s)) => {
-                        let val = BigDecimal::from_str(s)?;
-                        println!("[BigDecimal] value came from object '{o:?}' => {:?}", val);
-                        Ok(val)
-                    },
-
-                    _ => Err(InputValueError::from("Invalid number"))
-                }
             },
 
             _ => Err(InputValueError::expected_type(value)),
