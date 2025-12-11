860b8cc86174f6b784bf37d07f2e021b749c60c0
Luca Quaer <luca.quaer@ascatu.de>
2025-12-11 14:48:21 +0100

    feat: implement floating point parsting with BigDecimal by enabling serde_json/arbitrary_precision

--- src/types/external/big_decimal.rs
+++ src/types/external/big_decimal.rs
@@ -11,24 +11,31 @@ impl ScalarType for BigDecimal {
 
         match &value {
             Value::Number(n) => {
-                println!("[BigDecimal] value is a number");
+                // println!("[BigDecimal] value is a number");
 
-                if let Some(f) = n.as_f64() {
-                    let val = BigDecimal::try_from(f).map_err(InputValueError::custom)?;
-                    println!("[BigDecimal] value is a f64: {f} => {val:?}");
-                    return Ok(val);
-                }
+                // if let Some(f) = n.as_f64() {
+                //     let val = BigDecimal::try_from(f).map_err(InputValueError::custom)?;
+                //     println!("[BigDecimal] value is a f64: {f} => {val:?}");
+                //     return Ok(val);
+                // }
 
-                if let Some(f) = n.as_i64() {
-                    let val = BigDecimal::from(f);
-                    println!("[BigDecimal] value is a i64: {f} => {val:?}");
-                    return Ok(val);
-                }
+                // if let Some(f) = n.as_i64() {
+                //     let val = BigDecimal::from(f);
+                //     println!("[BigDecimal] value is a i64: {f} => {val:?}");
+                //     return Ok(val);
+                // }
 
-                let n2 = n.as_u64().unwrap();
-                let val = BigDecimal::from(n2);
+                // let n2 = n.as_u64().unwrap();
+                // let val = BigDecimal::from(n2);
 
-                println!("[BigDecimal] value is 'probably' a u64: {n2} => {val:?}");
+                // println!("[BigDecimal] value is 'probably' a u64: {n2} => {val:?}");
+
+                let val = BigDecimal::from_str(n.as_str())?;
+                println!(
+                    "[BigDecimal] value is a arbitrary-precision-num '{}' => {:?}",
+                    n.as_str(),
+                    val
+                );
 
                 // unwrap safe here, because we have check the other possibility
                 Ok(val)
